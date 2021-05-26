7a26dba2709d23c2c119027019802809fe65a270
zengchaoliang <pete_zeng@163.com>
2021-05-26 19:05:21 +0800

    增加Test注释

--- src/main/java/com/pete/testdemo/TestdemoApplication.java
+++ src/main/java/com/pete/testdemo/TestdemoApplication.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class TestdemoApplication {
-
+/***
+*TEST
+***/
     @GetMapping("/")
     public String index(){
         System.out.println("test");
